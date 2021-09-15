72363f5207e87107bba607c2383357496278f3d9
David Espinal <despinal0425@gmail.com>
2021-09-15 19:23:40 -0400

    added allow null to the migration file and change Sequelize paramerter to DataTypes

--- server/migrations/20210915230848-create-user.js
+++ server/migrations/20210915230848-create-user.js
@@ -1,45 +1,48 @@
 'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+  up: async (queryInterface, DataTypes) => {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: DataTypes.INTEGER
       },
       first_name: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       },
       last_name: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       },
       email: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING,
+        allowNull: false
       },
       role: {
-        type: Sequelize.ENUM('admin', 'user')
+        type: DataTypes.ENUM('admin', 'user')
       },
       salt: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING
       },
       password: {
-        type: Sequelize.STRING
+        type: DataTypes.STRING
       },
       last_login: {
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: DataTypes.DATE
       }
     });
   },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users');
+  down: async (queryInterface, DataTypes) => {
+    await queryInterface.dropTable('users');
   }
 };
