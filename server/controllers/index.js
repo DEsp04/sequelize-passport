b850896f275c0d52b3a5d95253d0a33388a7a31d
David Espinal <despinal0425@gmail.com>
2021-09-15 20:55:55 -0400

    added the framework and passport to the server

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,11 +3,28 @@ const { sequelize, User } = require('../models')
 
 
 
+const createUser = async (req, res) => {
+  
 
+}
 
+const getUserByID = async (req, res) => {
+  
+};
+
+const updateUser = async (req, res) => {
+  
+};
+
+const deleteUser = async (req, res) => {
+ 
+};
 
 
 
 module.exports = {
-  
+  createUser,
+  getUserByID,
+  updateUser,
+  deleteUser
 }
---
+++ server/server.js
@@ -0,0 +1,10 @@
+//Server
+const server = require("./app");
+
+
+//----------------------------End of Import-----------------
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
