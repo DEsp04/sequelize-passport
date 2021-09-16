a9314530977f4229a5ce5b327fd3530ff7274dbe
David Espinal <despinal0425@gmail.com>
2021-09-15 21:05:39 -0400

    added the passport library to the app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 //easier to have multple endpoint to test different things
 const express = require('express');
 
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
 //specific local host have access to the server
 const cors = require("cors");
 
@@ -23,5 +26,13 @@ app.use(cors());
 app.get("/", (req, res) => res.send("API is running!"));
 
 app.use("/api", userRoutes);
+
+//Initialize the PassportJS library:
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+
+
 
 module.exports = app;
